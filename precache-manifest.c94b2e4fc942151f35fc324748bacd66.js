47af404445ec29f767733c49f11fb80ad6d5899a
Luca Fabbri <l.fabbri@bopen.eu>
2020-12-07 15:58:02 +0100

    Updates

---
+++ precache-manifest.c94b2e4fc942151f35fc324748bacd66.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e36c8bad4f76dd0cc6eed9087b8c10c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "93e0d054714eb19e5b77",
+    "url": "./static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "d3c002767bdc62d66860",
+    "url": "./static/css/main.e6c0635a.chunk.css"
+  },
+  {
+    "revision": "93e0d054714eb19e5b77",
+    "url": "./static/js/2.b803733f.chunk.js"
+  },
+  {
+    "revision": "223e1043e6c92cd5b8490b603521d509",
+    "url": "./static/js/2.b803733f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c002767bdc62d66860",
+    "url": "./static/js/main.ac9fac70.chunk.js"
+  },
+  {
+    "revision": "49ff9ee768b0ad4cefde",
+    "url": "./static/js/runtime-main.7e55ea6c.js"
+  },
+  {
+    "revision": "a8695c4b0c652baac4a2291eed8148e6",
+    "url": "./static/media/B-Open.a8695c4b.svg"
+  },
+  {
+    "revision": "01b4354a9aa33b38dab5eefeb104c6f3",
+    "url": "./static/media/area-icon~bdUaMhse.01b4354a.svg"
+  }
+]);
